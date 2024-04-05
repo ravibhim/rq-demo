31a7667f36d1f0b7afe7714e3c7bdc19af3b67b9
Ravi Bhim <ravi.bhim@yahoo.com>
2024-04-05 16:59:31 +0530

    Using refetch on a user event

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,7 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+// Important Defaults - https://tanstack.com/query/latest/docs/framework/react/guides/important-defaults
 
 export const RQSuperHeroesPage = () => {
-  const { isLoading, data } = useQuery({
+  const [checked, setChecked] = useState(false);
+
+  const { isLoading, data, refetch } = useQuery({
     queryKey: ["rq-super-heroes"],
     queryFn: () =>
       fetch("http://localhost:4000/superheroes").then((res) => res.json()),
@@ -14,6 +19,15 @@ export const RQSuperHeroesPage = () => {
   return (
     <>
       <h2>RQ Super Heroes Page</h2>
+      Checked: {checked ? "true" : "false"}{" "}
+      <button
+        onClick={() => {
+          setChecked(!checked);
+          refetch();
+        }}
+      >
+        Toggle Checked
+      </button>
       {data.map((hero) => {
         return <div key={hero.id}>{hero.name}</div>;
       })}
